b7693cb77d9d423c0e2dea588316b1fa327d82eb
SimoneBr55 <39730275+SimoneBr55@users.noreply.github.com>
2022-04-21 13:53:41 +0000

    Edited via IntelliJ, with temp laptop.

--- java/src/superbasics/chronometerThreaded/TimerPanel.java
+++ java/src/superbasics/chronometerThreaded/TimerPanel.java
@@ -1,5 +1,42 @@
 package superbasics.chronometerThreaded;
 
 import javax.swing.*;
+import java.util.TimerTask;
+
 public class TimerPanel extends JPanel {
+    JLabel l_index;
+    JLabel l_time;
+    JButton start;
+    JButton stop;
+    JButton pause;
+    TimerLogic t;
+    public TimerPanel(){
+
+        t = new TimerLogic();
+        l_time = new JLabel("00:00.0", JLabel.CENTER);
+        pause = new JButton("PAUSE");
+        start = new JButton("START");
+        stop = new JButton("STOP");
+
+        start.addActionListener(new starts());
+        pause.addActionListener(new starts());
+        stop.addActionListener(new starts());
+
+        add(l_time);
+        add(start);
+        add(pause);
+        add(stop);
+
+        java.util.Timer updateTimer = new java.util.Timer();
+        updateTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run(){
+                t.update(int_min, int_sec, int_mil);
+            }
+        },0,100);
+    }
+
+    public void update(long minutes, long seconds, long milliseconds) {
+        String sec = Integer.toString();
+    }
 }
